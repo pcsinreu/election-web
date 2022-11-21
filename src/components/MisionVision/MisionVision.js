2208eddb2adab8fea97518be4309b5f61995285c
‘nreupomckinsey’ <naldo_reupo-musayon@mckinsey.com>
2022-11-21 09:19:00 -0500

    feat update vision and mision

--- src/components/MisionVision/MisionVision.js
+++ src/components/MisionVision/MisionVision.js
@@ -4,9 +4,9 @@ const MisionVision = () =>{
     return(
         <div className="MisionVision">
             <h2>Misión</h2>
-            <p>idem</p>
+            <p>Con nuestra página mejorar nuestro Perú a base de una concientización ciudadana, para que elijamos a nuestros representantes correctamente, y estos sean líderes verdaderos que guíen a la meta en común, el desarrollo en conjunto como nación</p>
             <h2>Visión</h2>
-            <p>idem</p>
+            <p>Esperemos lograr un alcance nacional, para que se complemente con nuestra misión y así nuestra página sea un instrumento que funcione de información y preparación en todo tipo de elecciones dentro de nuestro país</p>
         </div>
     )
 
